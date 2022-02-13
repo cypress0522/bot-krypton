1f639f19e9a9c5586b2501945ef8ed35d7b10725
Hsiang Nianian <44714368+ArbETaperingsky@users.noreply.github.com>
2022-02-13 23:44:06 +0800

    Create index.php

---
+++ api/yiyan/index.php
@@ -0,0 +1,17 @@
+<?php 
+//获取句子文件的绝对路径
+$path = dirname(__FILE__);
+$file = file($path."/hitokoto.txt");
+//这里是以单个php文件为位置的路径
+
+//随机读取一行
+$arr  = mt_rand( 0, count( $file ) - 1 );
+$content  = trim($file[$arr]);
+
+//格式化判断，输出js或纯文本
+if ($_GET['encode'] === 'js') {
+    echo "function hitokoto(){document.write('" . $content ."');}";
+} else {
+    echo $content;
+}
+?>
